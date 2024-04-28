57f7ec2fa4f3cbcda2fdee289f766df58912f5c4
André de Queiroz <andrepatrinicola@gmail.com>
2024-04-28 10:46:17 -0300

    add cors

--- Projeto/backend/mapa_organicos/config/settings.py
+++ Projeto/backend/mapa_organicos/config/settings.py
@@ -46,6 +46,11 @@ INSTALLED_APPS = [
     "rest_framework",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173"
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
